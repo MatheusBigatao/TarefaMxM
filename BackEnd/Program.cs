3b61e9af6c78a6cf3b4ba335355501bdeb393008
matheusbigatao <matheus.bmartinelli@gmail.com>
2024-04-28 00:42:28 -0300

    fix: permitir fazer requisições API pelo Front

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -25,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
